b1f4f7aa294857e6fed1c874e9daf479d0dcb61e
TakenMist <92009569+TakenMist@users.noreply.github.com>
2024-04-29 19:48:16 -0400

    Create solucionMatrices.cpp
    Posible solucion.
    Usar como main.cpp en el onlinegdb

---
+++ solucionMatrices.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+void multiplication(int **matriz, int columnas, int filas, int **matrizResultado){
+    for(int i = 0; i < filas; i++){
+        for(int j = 0; j < columnas; j++){
+            matrizResultado[i][j] = 0;
+            for(int n = 0; n < columnas; n++){
+                matrizResultado[i][j] += matriz[i][n]*matriz[n][j];
+            }
+        }
+    }
+    for(int i = 0; i < filas; i++){
+        for(int j = 0; j < columnas; j++)
+            matriz[i][j] = matrizResultado[i][j];
+    }
+}
+
+int main(){
+    int filas, columnas;
+    do{
+        cout << "Ingrese n° de filas: ";
+        cin >> filas;
+        cout << "Ingrese n° de columnas: ";
+        cin >> columnas;
+        if(filas != columnas)
+            cout << "Dimensiones no congruentes para multiplicacion.\n";
+    }while(filas < 0 || columnas < 0 || filas != columnas);
+
+    // Asigna el espacio para las filas de la matriz
+    int **matriz = (int**)malloc(filas * sizeof(int*));
+    // Asigna el espacio para las columnas de la matriz
+    for(int i = 0; i < columnas; i++){
+        matriz[i] = (int*)malloc(columnas * sizeof(int));
+    }
+
+    // Asigna el espacio para las filas de la matriz resultado
+    int **matrizResultado = (int**)malloc(filas * sizeof(int*));
+    // Asigna el espacio para las columnas de la matriz resultado
+    for(int i = 0; i < columnas; i++){
+        matrizResultado[i] = (int*)malloc(columnas * sizeof(int));
+    }
+
+    // Asigna valores al inicio de la matriz
+    for(int i = 0; i < filas; i++){
+        for(int j = 0; j < columnas; j++){
+            cout << "Ingrese valor posicion [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+            matrizResultado[i][j] = 0;
+        }
+        cout << "\n";
+    }
+    int x;
+    do{
+        do{
+        cout << "Para iterar, ingrese 1.\n";
+        cout << "Para terminar, ingrese 0.\n";
+        cin >> x;
+        } while(x != 0 && x != 1);
+        multiplication(matriz, columnas, filas, matrizResultado);
+        // Muestra los valores del resultado
+        for(int i = 0; i < filas; i++){
+            for(int j = 0; j < columnas; j++){
+                cout << matrizResultado[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+    } while(x == 1);
+    cout << "\n\n";
+
+    for(int i = 0; i < columnas; i++){
+        free(matrizResultado[i]);
+    }
+    for(int i = 0; i < columnas; i++){
+        free(matriz[i]);
+    }
+    free(matriz);
+    free(matrizResultado);
+
+    return 0;
+}
